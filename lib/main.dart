05e8de80efad962434699a3dac4202f012c649e4
Saurav Kumar <sauravk7077@gmail.com>
2020-12-04 09:59:43 +0530

    added basic files

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/screens/wrapper.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: Wrapper());
+  }
+}
